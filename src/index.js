b37edc753a9eefb3cb4038411133b662091cc079
katpimo <kat.pimo@gmail.com>
2020-02-13 12:52:10 -0500

    Funcionalidad de boton empezar, cajas de texto y botones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+
+//funcion de boton sin onclick
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () =>{
+    const pantalla1 = document.getElementById("pantalla1")
+    pantalla1.classList.add("ocultar")
+    const pantalla2 = document.getElementById("pantalla2")
+    pantalla2.classList.remove("ocultar")
+});
