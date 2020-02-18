a97d2cea438d43c85d19906a56bfdf37851bf41a
katpimo <kat.pimo@gmail.com>
2020-02-18 08:21:12 -0500

    funcionalidad de botones: codificar, decodificar y limpiar

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const btn = document.getElementById('btn');
 btn.addEventListener('click', function() {
     const pantalla1 = document.getElementById("pantalla1")
     pantalla1.classList.add("ocultar")
+    document.getElementById("dplz").value='0'; // valor por defecto al desplazamiento
     const pantalla2 = document.getElementById("pantalla2")
     pantalla2.classList.remove("ocultar")
 });
@@ -20,20 +21,25 @@ const cdf = document.getElementById('cdf');
     }
 );
 
-function cipher_x(ingresa, dplz) {
-	let salida = '';
-	let sinCifrado; //donde se guarda el codigo ascii de una letra
-	let cifrado; //codigo ascii resultante luego de sumarle paso
-	//por cada letra que se ingresa
-	for (let i = 0; i < ingresa.length; i++) {
-        sinCifrado = ingresa[i].charCodeAt();
-        if (ingresa[i].charCodeAt()===32){
-            salida = salida.concat(" "); 
+function cipher_x(entrada, paso) {
+    var salida = '';
+    var sinCifrado; //donde se guarda el codigo ascii de una letra
+    var cifrado; //codigo ascii resultante luego de sumarle paso
+    //por cada letra de la entrada
+
+    for (var c = 0; c < entrada.length; c++) {
+        sinCifrado = entrada[c].charCodeAt(); //obtenemos el número ASCII
+        if (sinCifrado==32) {
+            salida=salida.concat(" ");
+        } else if (sinCifrado >= 65 && sinCifrado <= 90) {
+            cifrado = (sinCifrado - 65 + paso)%26 + 65; //desplazamos de lugar la letra al sumarle paso
+            salida = salida.concat(String.fromCharCode(cifrado)); //convertimos el nuevo codigo a string y concatenamos
+        } else if (sinCifrado >= 97 && sinCifrado <= 122) {
+            cifrado = (sinCifrado - 97 + paso)%26 + 97; //desplazamos de lugar la letra al sumarle paso
+            salida = salida.concat(String.fromCharCode(cifrado)); //convertimos el nuevo codigo a string y concatenamos
         }
-        cifrado = (sinCifrado - 65 + dplz)%26 + 65; //desplazamos de lugar la letra al sumarle paso
-        salida = salida.concat(String.fromCharCode(cifrado)); //convertimos el nuevo codigo a string y concatenamos(construir la palabra,juntar letra por letra)
-	}
-	return salida;
+    }
+    return salida;
 }
 
 //para decodificar aplicar string.fromCharcode();
@@ -50,18 +56,38 @@ const dcf = document.getElementById('dcf');
     }
 );
 
-function cipher_y(ingresa, dplz) {
-	let salida = '';
-	let cifrado; //donde se guarda el codigo ascii de una letra
-	let sinCifrado; //codigo ascii resultante luego de sumarle paso
-	//por cada letra que se ingresa
-	for (let i = 0; i < ingresa.length; i++) {
-        cifrado = ingresa[i].charCodeAt();
-        if (ingresa[i].charCodeAt()===32){
-            salida = salida.concat(" "); 
+function cipher_y(entrada, paso) {
+    var salida = '';
+    var sinCifrado; //donde se guarda el codigo ascii de una letra
+    var cifrado; //codigo ascii resultante luego de sumarle paso
+    //por cada letra de la entrada
+
+    for (var c = 0; c < entrada.length; c++) {
+        sinCifrado = entrada[c].charCodeAt(); //obtenemos el número ASCII
+        if (sinCifrado==32) {
+            salida=salida.concat(" ");
+        } else if (sinCifrado >= 65 && sinCifrado <= 90) {
+            cifrado = (sinCifrado - 65 + (26-paso%26))%26 + 65; //desplazamos de lugar la letra al sumarle paso
+            salida = salida.concat(String.fromCharCode(cifrado)); //convertimos el nuevo codigo a string y concatenamos
+        } else if (sinCifrado >= 97 && sinCifrado <= 122) {
+            cifrado = (sinCifrado - 97 + (26-paso%26))%26 + 97; //desplazamos de lugar la letra al sumarle paso
+            salida = salida.concat(String.fromCharCode(cifrado)); //convertimos el nuevo codigo a string y concatenamos
         }
-        sinCifrado = (cifrado - 65 + (26-dplz))%26 + 65; //desplazamos de lugar la letra al sumarle paso
-        salida = salida.concat(String.fromCharCode(sinCifrado)); //convertimos el nuevo codigo a string y concatenamos(construir la palabra,juntar letra por letra)
-	}
-	return salida;
+    }
+    return salida;
+}
+
+
+
+//*funcionalidad a mi boton de limpiar
+const limpiar = document.getElementById('lmp');
+    limpiar.addEventListener('click', function (){
+    LimpiarBox();
+    }
+);
+
+function LimpiarBox (){
+    document.getElementById("dplz").value='0';
+    document.getElementById("ingresa").value='';
+    document.getElementById("salida").innerHTML='';
 }
